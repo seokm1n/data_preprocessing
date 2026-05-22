24768cee3be17300d1e9337d6dbcc42f3d52881d
seokm1n <suckminzzz@gmail.com>
2026-05-22 18:23:11 +0900

    filtering

--- python_basic/20260522/네이버뉴스엑셀파일_읽기.py
+++ python_basic/20260522/네이버뉴스엑셀파일_읽기.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import re
+
 newsdf = pd.read_excel("navernews.xlsx")
 # print(newsdf)
 # print(newsdf["뉴스 제목"][0])
-for item in newsdf['뉴스 제목']:
-    kor_item = re.sub(r'[^ㄱ-힣\s]+','', item)
-    print(kor_item)
-    print("="*55)
+# for item in newsdf['뉴스 제목']:
+#     print(re.sub(r'[^ㄱ-힣\s]+','', item))
+#     print("="*55)
+print(newsdf)
+print("="*80)
+
+def newstitlefiltering(arg):
+    # print('arg : ', arg)
+    # print(re.sub(r'[^ㄱ-힣\s]+', '', arg))
+    return re.sub(r'[^ㄱ-힣\s]+', '', arg)
+
+
+newsdf['뉴스 제목']=newsdf['뉴스 제목'].apply(newstitlefiltering)
+print(newsdf)
+newsdf.to_excel('navernews_filtering.xlsx', index=False)
