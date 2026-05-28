e1b77a5c144bffeb4a7f52f9c2c14b482bf96830
15 <suckminzzz@gmail.com>
2026-05-28 14:30:42 +0900

    pop

--- 20260528/pandas_리뷰.py
+++ 20260528/pandas_리뷰.py
@@ -2,22 +2,22 @@ import numpy as np
 import pandas as pd
 
 # 사전을 활용해서 Dataframe 객체 생성
-dictdata = {'Hong':[90,80,70,60,75], 'Kim':[85,95,65,55,75], 'Park':[88, 93, 75, 72,75], 'Lee':[55,66,77,92,75] }
+dictdata = {'Hong': [90, 80, 70, 60, 75], 'Kim': [85, 95, 65, 55, 75], 'Park': [88, 93, 75, 72, 75],
+            'Lee': [55, 66, 77, 92, 75]}
 # 위 사전을 활용서 Dataframe  객체 생성
-scoredf = pd.DataFrame( dictdata , index=['kor','eng','math','music','sci'])
+scoredf = pd.DataFrame(dictdata, index=['kor', 'eng', 'math', 'music', 'sci'])
 print(scoredf)
-scoredf.rename( index={'music':'음악'} , inplace=True)
+scoredf.rename(index={'music': '음악'}, inplace=True)
 print(scoredf)
-scoredf.rename(columns={'Park':"신"} , inplace=True)
+scoredf.rename(columns={'Park': "신"}, inplace=True)
 print(scoredf)
 
-sortdf = scoredf[ ['Hong','Kim','Lee','신'] ].copy()
+sortdf = scoredf[['Hong', 'Kim', 'Lee', '신']].copy()
 print(sortdf)
-print("="*80)
-print( scoredf.iloc[ 1:2, :]  )
+print("=" * 80)
+print(scoredf.iloc[1:2, :])
 # fancy index ==> 추출하고자 하는 idex의 배열을 전달
-print( scoredf.iloc[ [1,3], : ] )
-
+print(scoredf.iloc[[1, 3], :])
 
 # timedf = pd.DataFrame( np.arange(1,51) , index= pd.date_range('2026.12.28',periods=50,freq='D') ,
 #               columns=['data'] )
--- 20260528/pandas_함수적용_예제.py
+++ 20260528/pandas_함수적용_예제.py
@@ -1,34 +1,35 @@
 import numpy as np
 import pandas as pd
 
-arr = np.arange(1,10).reshape(3,3)
+arr = np.arange(1, 10).reshape(3, 3)
 print(arr)
-print( arr.mean(axis=0) )  # 제공해주는 집계(통계) ==> sum, min, max, mean
+print(arr.mean(axis=0))  # 제공해주는 집계(통계) ==> sum, min, max, mean
 
-mydf = pd.DataFrame(arr , columns=['a','b','c'] )
+mydf = pd.DataFrame(arr, columns=['a', 'b', 'c'])
 print(mydf)
 print(mydf['a'].mean(), mydf['a'].max())
 
 
 def MyAddFuc(arg):
-    return arg+5
+    return arg + 5
 
-#result = MyAddFuc(7)
-#f = lambda x : x+5  # 이름이 없는 함수 ==> lambda 함수(객체)
-result = (lambda x : x+5)(7)
+
+# result = MyAddFuc(7)
+# f = lambda x : x+5  # 이름이 없는 함수 ==> lambda 함수(객체)
+result = (lambda x: x + 5)(7)
 print(result)
 
-print( np.power(5,3) )  # ==> 제곱승 연산
-print( 5**3 )
-
+print(np.power(5, 3))  # ==> 제곱승 연산
+print(5 ** 3)
 
 # 사전을 활용해서 Dataframe 객체 생성
-dictdata = {'Hong':[90,80,70,60,75], 'Kim':[85,95,65,55,75], 'Park':[88, 93, 75, 72,75], 'Lee':[55,66,77,92,75] }
+dictdata = {'Hong': [90, 80, 70, 60, 75], 'Kim': [85, 95, 65, 55, 75], 'Park': [88, 93, 75, 72, 75],
+            'Lee': [55, 66, 77, 92, 75]}
 # 위 사전을 활용서 Dataframe  객체 생성
-scoredf = pd.DataFrame( dictdata , index=['kor','eng','math','music','sci'])
+scoredf = pd.DataFrame(dictdata, index=['kor', 'eng', 'math', 'music', 'sci'])
 print(scoredf)
 
-scoredf['성별'] = ['male', 'female', 'female','male','female']
+scoredf['성별'] = ['male', 'female', 'female', 'male', 'female']
 print(scoredf)
 scoredf.info()
 # Dataframe 또는 Series 의 각 요소에 특정 함수를 일괄 적용시켜서
@@ -42,12 +43,14 @@ scoredf.info()
 #         return 0
 #
 
-scoredf['성별'] = scoredf['성별'].map( {'male':0,'female':1} )
+scoredf['성별'] = scoredf['성별'].map({'male': 0, 'female': 1})
 print(scoredf)
 
+
 def ScoreIncrese(arg):
-    return arg+5
+    return arg + 5
+
 
 scoredf['Hong'] = scoredf['Hong'].apply(ScoreIncrese)
 print(scoredf)
-#print( scoredf['Hong'].apply(lambda x : x+5) )
+# print( scoredf['Hong'].apply(lambda x : x+5) )
